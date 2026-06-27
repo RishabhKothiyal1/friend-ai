5f5aa3b21beee468ad40df0d030bbd907bc470f3
Rishabh Kothiyal <rishabhkothiyal2004@gmail.com>
2026-06-27 14:22:19 +0530

    chore: remove unused Google Workspace Hubs, legacy Python code, and Firebase dependency (YAGNI cleanup)

--- src/components/demo.tsx
+++
@@ -1,9 +0,0 @@
-import MotionButton from "../components/ui/motion-button";
-
-export default function Demo() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-background">
-      <MotionButton label="Get Started" />
-    </div>
-  );
-}
